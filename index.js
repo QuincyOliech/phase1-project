4c2a1a5cb62a13f3e00396a35dcff0f75cdebdd6
Linet Oliech <linetaoliech@gmail.com>
2022-12-15 23:43:15 +0300

    declared variables in my js file

---
+++ index.js
@@ -0,0 +1,14 @@
+// declaring variables
+const menu = document.getElementById('mobileMenu');
+const menuLinks = document.querySelector('.navbarMenu')
+const dividedList = document.querySelector("#products");
+const productImage = document.querySelector("#picture");
+const brandName = document.querySelector("#brand");
+const itemDescription = document.querySelector("#description");
+const productPrice = document.querySelector("#price");
+const productPhoto = document.querySelector("#photo");
+const productNumber=document.querySelector('#product-num')
+const purchaseButton = document.querySelector("#buy-item");
+const searchBar = document.querySelector('#searchbar');
+let id=0
+let totalNumberOfItemsSold=0
